4700153da4c28157f52a3e8037f7699e1693d95f
Jorge Candeias <candeias@fastmail.fm>
2018-12-02 01:12:30 +0000

    Updated coverage script.

--- tools/coverage.ps1
+++ tools/coverage.ps1
@@ -1,28 +1,13 @@
-﻿# install minicover
+﻿cd tools
 dotnet restore
-
-# instrument assemblies inside 'test' folder to detect hits for source files inside 'src' folder
 dotnet minicover instrument --workdir ../ --assemblies test/**/bin/**/*.dll --sources src/**/*.cs
-
-# rem Reset hits count in case minicover was run for this project
 dotnet minicover reset --workdir ../
-
-$Location = Get-Location
-cd ..
-
-Get-ChildItem -Filter "*tests*.csproj" -Recurse | %{
-    dotnet publish $_.FullName
+cd ..\test
+gci -Filter *.csproj -Recurse | %{
     dotnet test $_.FullName
 }
-
-cd $Location
-
-# uninstrument assemblies, it's important if you're going to publish or deploy build outputs
+cd ..\tools
 dotnet minicover uninstrument --workdir ../
-
-# create html reports inside folder coverage-html
 dotnet minicover htmlreport --workdir ../ --threshold 90
-
-# print console report
-# this command returns failure if the coverage is lower than the threshold
 dotnet minicover report --workdir ../ --threshold 90
+cd ..
