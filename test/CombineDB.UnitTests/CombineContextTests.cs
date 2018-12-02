271ab6212d7826e218c6facfbec6b126aa46af8f
Jorge Candeias <candeias@fastmail.fm>
2018-12-02 00:34:54 +0000

    Fixed coverage script.

---
+++ test/CombineDB.UnitTests/CombineContextTests.cs
@@ -0,0 +1,16 @@
+﻿using CombineDB.Core;
+using NUnit.Framework;
+using System.Linq;
+
+namespace CombineDB.UnitTests
+{
+    [TestFixture]
+    public class CombineContextTests
+    {
+        [Test]
+        public void CombineContext_Implements_ICombineContext()
+        {
+            Assert.True(typeof(CombineContext).GetInterfaces().Contains(typeof(ICombineContext)));
+        }
+    }
+}
